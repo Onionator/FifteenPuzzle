efb5d2886c88c54d20c841910336137e373c94d5
Student <example@example.com>
2019-10-08 10:35:58 -0700

    add functionality for the player to play the game, count the moves, and check for a win.

--- app/src/main/java/com/example/fifteenpuzzle/MainActivity.java
+++ app/src/main/java/com/example/fifteenpuzzle/MainActivity.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private SlidePuzzle puzzle;
     private int[][] board;
+    private Button[][] buttons = new Button[4][4];
+    private int moves = 0;
+    private TextView textViewMoveCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +27,102 @@ public class MainActivity extends AppCompatActivity {
 
         puzzle = new SlidePuzzle();
         board = puzzle.getBoard();
+        textViewMoveCounter = findViewById(R.id.counter);
+
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
 
+//                String buttonId = "button" + i + j;
+//                Button button = (Button)findViewById(getResources().getIdentifier(buttonId, "id", getPackageName()));
+//                button.setText(Integer.toString(board[i][j]));
+//                button.setOnClickListener((View.OnClickListener) this);
+
                 String buttonId = "button" + i + j;
-                Button button = (Button)findViewById(getResources().getIdentifier(buttonId, "id",getPackageName()));
-                button.setText(Integer.toString(board[i][j]));
+                int resourceId = getResources().getIdentifier(buttonId, "id", getPackageName());
+                buttons[i][j] = findViewById(resourceId);
+                buttons[i][j].setText(Integer.toString(board[i][j]));
+                buttons[i][j].setOnClickListener(this);
             }
+        }
+
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        Log.i("v's number is", ((Button) v).getText().toString());
+
+        int[] clickedButton = puzzle.find(Integer.valueOf(((Button) v).getText().toString()));
+
+        boolean checkMoves = true;
+        while (checkMoves) {
+            if (clickedButton[0] > 0) {
+                if (board[clickedButton[0] - 1][clickedButton[1]] == 0) {
+                    Log.i("The number can move up", "move up");
+                    puzzle.moveUp(board[clickedButton[0]][clickedButton[1]]);
+                    updateBoard();
+                    ((Button) v).setText("0");
+                    break;
+                }
+            }
+            if (clickedButton[1] > 0) {
+                if (board[clickedButton[0]][clickedButton[1] - 1] == 0) {
+                    Log.i("The number can move left", "move left");
+                    puzzle.moveLeft(board[clickedButton[0]][clickedButton[1]]);
+                    updateBoard();
+                    ((Button) v).setText("0");
+                    break;
+                }
+            }
+            if (clickedButton[1] + 1 < puzzle.boardLength) {
+                if (board[clickedButton[0]][clickedButton[1] + 1] == 0) {
+                    Log.i("The number can move right", "move right");
+                    puzzle.moveRight(board[clickedButton[0]][clickedButton[1]]);
+                    updateBoard();
+                    ((Button) v).setText("0");
+                    break;
+                }
+            }
+            if (clickedButton[0] + 1 < puzzle.boardLength) {
+                if (board[clickedButton[0] + 1][clickedButton[1]] == 0) {
+                    Log.i("The number can move down", "move down");
+                    puzzle.moveDown(board[clickedButton[0]][clickedButton[1]]);
+                    updateBoard();
+                    ((Button) v).setText("0");
+                    break;
+                }
+            }
+
+            Log.i("The number cannot move", "Try again");
+            checkMoves = false;
         }
 
     }
 
-
-    private void drawNumbers(View view) {
-
-
+    public void updateBoard() {
+        board = puzzle.getBoard();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                String buttonId = "button" + i + j;
+                int resourceId = getResources().getIdentifier(buttonId, "id", getPackageName());
+                buttons[i][j] = findViewById(resourceId);
+                buttons[i][j].setText(Integer.toString(board[i][j]));
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+        // update move counter
+        moves++;
+        textViewMoveCounter.setText(String.valueOf(moves));
+        
+        if (Arrays.equals(board, puzzle.finishedPuzzle)) {
+            // if the game is finished
+            Toast.makeText(this, "You Win!", Toast.LENGTH_SHORT).show();
+        }
     }
+
+    
+
 }
